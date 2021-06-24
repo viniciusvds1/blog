3ffd3af393374f39754192cb73b1e56a016bf4b6
Joao Vinicius <jvinicius@detro.rj.gov.br>
2021-06-24 11:39:47 -0300

    Blog de noticias sobre programaçao

---
+++ helpers/url.php
@@ -0,0 +1,3 @@
+<?php
+  // HELPER PARA LINKAR IMAGENS E STILOS
+  $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']. '?') . '/';
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>Blog do Dev &copy; 2021</p>
+</footer>
+</body>
+</html>
